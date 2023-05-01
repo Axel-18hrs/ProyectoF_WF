d991c119e682b8b6ea24cb2cd616b5397c51b9bb
axel1 <axel1@AXEL>
2023-05-01 11:48:53 -0600

    Agregar archivos de proyecto.

---
+++ ProyectoF_WF/Beginning.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace ProyectoF_WF
+{
+    partial class fmBeginning
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(fmBeginning));
+            this.pnlBEGLoginOrSignIn = new System.Windows.Forms.Panel();
+            this.pbBEGAvatarIco = new System.Windows.Forms.PictureBox();
+            this.lblBEGNameOfUser = new System.Windows.Forms.Label();
+            this.txtBEGNameOfUser = new System.Windows.Forms.TextBox();
+            this.txtBEGPassword = new System.Windows.Forms.TextBox();
+            this.lblBEGPassword = new System.Windows.Forms.Label();
+            this.btnBEGbeginning = new System.Windows.Forms.Button();
+            this.lnklblBEGAccessAsAdministrator = new System.Windows.Forms.LinkLabel();
+            this.pnlBEGLoginOrSignIn.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBEGAvatarIco)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlBEGLoginOrSignIn
+            // 
+            this.pnlBEGLoginOrSignIn.BackColor = System.Drawing.Color.DimGray;
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.lnklblBEGAccessAsAdministrator);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.btnBEGbeginning);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.txtBEGPassword);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.lblBEGPassword);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.txtBEGNameOfUser);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.lblBEGNameOfUser);
+            this.pnlBEGLoginOrSignIn.Controls.Add(this.pbBEGAvatarIco);
+            this.pnlBEGLoginOrSignIn.Location = new System.Drawing.Point(371, 79);
+            this.pnlBEGLoginOrSignIn.Name = "pnlBEGLoginOrSignIn";
+            this.pnlBEGLoginOrSignIn.Size = new System.Drawing.Size(476, 462);
+            this.pnlBEGLoginOrSignIn.TabIndex = 0;
+            // 
+            // pbBEGAvatarIco
+            // 
+            this.pbBEGAvatarIco.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pbBEGAvatarIco.BackColor = System.Drawing.Color.Transparent;
+            this.pbBEGAvatarIco.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pbBEGAvatarIco.BackgroundImage")));
+            this.pbBEGAvatarIco.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pbBEGAvatarIco.Location = new System.Drawing.Point(163, 15);
+            this.pbBEGAvatarIco.Name = "pbBEGAvatarIco";
+            this.pbBEGAvatarIco.Size = new System.Drawing.Size(178, 157);
+            this.pbBEGAvatarIco.TabIndex = 1;
+            this.pbBEGAvatarIco.TabStop = false;
+            // 
+            // lblBEGNameOfUser
+            // 
+            this.lblBEGNameOfUser.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBEGNameOfUser.AutoSize = true;
+            this.lblBEGNameOfUser.BackColor = System.Drawing.Color.Transparent;
+            this.lblBEGNameOfUser.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBEGNameOfUser.Location = new System.Drawing.Point(170, 175);
+            this.lblBEGNameOfUser.Name = "lblBEGNameOfUser";
+            this.lblBEGNameOfUser.Size = new System.Drawing.Size(141, 23);
+            this.lblBEGNameOfUser.TabIndex = 2;
+            this.lblBEGNameOfUser.Text = "Name of user";
+            // 
+            // txtBEGNameOfUser
+            // 
+            this.txtBEGNameOfUser.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBEGNameOfUser.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBEGNameOfUser.Location = new System.Drawing.Point(110, 222);
+            this.txtBEGNameOfUser.Name = "txtBEGNameOfUser";
+            this.txtBEGNameOfUser.Size = new System.Drawing.Size(281, 28);
+            this.txtBEGNameOfUser.TabIndex = 3;
+            // 
+            // txtBEGPassword
+            // 
+            this.txtBEGPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtBEGPassword.Font = new System.Drawing.Font("Arial Narrow", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtBEGPassword.Location = new System.Drawing.Point(110, 311);
+            this.txtBEGPassword.Name = "txtBEGPassword";
+            this.txtBEGPassword.Size = new System.Drawing.Size(281, 28);
+            this.txtBEGPassword.TabIndex = 5;
+            // 
+            // lblBEGPassword
+            // 
+            this.lblBEGPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblBEGPassword.AutoSize = true;
+            this.lblBEGPassword.BackColor = System.Drawing.Color.Transparent;
+            this.lblBEGPassword.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBEGPassword.Location = new System.Drawing.Point(187, 282);
+            this.lblBEGPassword.Name = "lblBEGPassword";
+            this.lblBEGPassword.Size = new System.Drawing.Size(103, 23);
+            this.lblBEGPassword.TabIndex = 4;
+            this.lblBEGPassword.Text = "Password";
+            // 
+            // btnBEGbeginning
+            // 
+            this.btnBEGbeginning.BackColor = System.Drawing.Color.LightCoral;
+            this.btnBEGbeginning.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnBEGbeginning.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBEGbeginning.Location = new System.Drawing.Point(146, 363);
+            this.btnBEGbeginning.Name = "btnBEGbeginning";
+            this.btnBEGbeginning.Size = new System.Drawing.Size(195, 52);
+            this.btnBEGbeginning.TabIndex = 6;
+            this.btnBEGbeginning.Text = "Access or Register";
+            this.btnBEGbeginning.UseVisualStyleBackColor = false;
+            // 
+            // lnklblBEGAccessAsAdministrator
+            // 
+            this.lnklblBEGAccessAsAdministrator.AutoSize = true;
+            this.lnklblBEGAccessAsAdministrator.Location = new System.Drawing.Point(164, 427);
+            this.lnklblBEGAccessAsAdministrator.Name = "lnklblBEGAccessAsAdministrator";
+            this.lnklblBEGAccessAsAdministrator.Size = new System.Drawing.Size(150, 16);
+            this.lnklblBEGAccessAsAdministrator.TabIndex = 7;
+            this.lnklblBEGAccessAsAdministrator.TabStop = true;
+            this.lnklblBEGAccessAsAdministrator.Text = "Access as administrator";
+            this.lnklblBEGAccessAsAdministrator.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnklblBEGAccessAsAdministrator_LinkClicked);
+            // 
+            // fmBeginning
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Orchid;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1370, 553);
+            this.Controls.Add(this.pnlBEGLoginOrSignIn);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "fmBeginning";
+            this.Text = "Login or Registration";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlBEGLoginOrSignIn.ResumeLayout(false);
+            this.pnlBEGLoginOrSignIn.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbBEGAvatarIco)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlBEGLoginOrSignIn;
+        private System.Windows.Forms.PictureBox pbBEGAvatarIco;
+        private System.Windows.Forms.Label lblBEGNameOfUser;
+        private System.Windows.Forms.TextBox txtBEGNameOfUser;
+        private System.Windows.Forms.TextBox txtBEGPassword;
+        private System.Windows.Forms.Label lblBEGPassword;
+        private System.Windows.Forms.Button btnBEGbeginning;
+        private System.Windows.Forms.LinkLabel lnklblBEGAccessAsAdministrator;
+    }
+}
+
---
+++ ProyectoF_WF/Beginning.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using NAudio;
+using LibVLCSharp;
+using LibVLCSharp.Shared;
+using System.Reflection.Emit;
+
+namespace ProyectoF_WF
+{
+    public partial class fmBeginning : Form
+    {
+        // Uso de rutas relativas en lugar de las absolutas para una mejor practica
+        public fmBeginning()
+        {
+            InitializeComponent();
+            // Alineamiento del panel respecto al tamaño del formulario
+            pnlBEGLoginOrSignIn.BackColor = Color.FromArgb(150, Color.Violet);
+            pnlBEGLoginOrSignIn.Location = new Point((this.ClientSize.Width - pnlBEGLoginOrSignIn.Width) / 2, (this.ClientSize.Height - pnlBEGLoginOrSignIn.Height) / 2);
+
+            // Posicionamiento del picturebox respecto al panel
+            pbBEGAvatarIco.Anchor = AnchorStyles.None;
+            pbBEGAvatarIco.Dock = DockStyle.Top;
+            pbBEGAvatarIco.Width = 200;
+            pbBEGAvatarIco.AutoSize = true;
+            pbBEGAvatarIco.Margin = new Padding((pnlBEGLoginOrSignIn.ClientSize.Width - pbBEGAvatarIco.Width) / 2, 10, 0, 0);
+
+            // Posición del label debajo del picturebox y a su vez el textbox
+            lblBEGNameOfUser.Top = pbBEGAvatarIco.Bottom + 10;
+            lblBEGNameOfUser.Left = (pnlBEGLoginOrSignIn.Width - lblBEGNameOfUser.Width) / 2;
+
+            txtBEGNameOfUser.Top = lblBEGNameOfUser.Bottom + 10;
+            txtBEGNameOfUser.Left = (pnlBEGLoginOrSignIn.Width - txtBEGNameOfUser.Width) / 2;
+
+            // mismo patron pero debajo del textbox inicialmente
+            lblBEGPassword.Top = txtBEGNameOfUser.Bottom + 10;
+            lblBEGPassword.Left = (pnlBEGLoginOrSignIn.Width - lblBEGPassword.Width) / 2;
+
+            txtBEGPassword.Top = lblBEGPassword.Bottom + 10;
+            txtBEGPassword.Left = (pnlBEGLoginOrSignIn.Width - txtBEGPassword.Width) / 2;
+
+            // Boton de inicio
+            btnBEGbeginning.Top = txtBEGPassword.Bottom + 10;
+            btnBEGbeginning.Left = (pnlBEGLoginOrSignIn.Width - btnBEGbeginning.Width) / 2;
+
+            lnklblBEGAccessAsAdministrator.Top = btnBEGbeginning.Bottom + 8;
+            lnklblBEGAccessAsAdministrator.Left = (pnlBEGLoginOrSignIn.Width - lnklblBEGAccessAsAdministrator.Width) / 2;
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+        // Evento del link label por donde ocultaremos este formulario y mostraremos otro
+        private void lnklblBEGAccessAsAdministrator_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            this.Hide();
+            DataOfUser dataOfUser = new DataOfUser();
+            dataOfUser.StartPosition = FormStartPosition.Manual;
+            dataOfUser.Location = this.Location;
+            dataOfUser.ShowDialog();
+
+            
+        }
+    }
+}
